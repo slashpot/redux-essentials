7db8cfb655ff5c450d4a2741d0b465a03ddd9b1d
Eric Lin <tttttt123465@gmail.com>
2024-09-29 11:34:22 +0800

    add types

--- src/app/store.ts
+++ src/app/store.ts
@@ -18,3 +18,7 @@ export const store = configureStore({
     counter: counterReducer
   }
 })
+
+export type AppStore = typeof store
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
