4d79979af3c44164930685a29dcc6818ab924b34
Eric Lin <tttttt123465@gmail.com>
2024-09-29 12:04:02 +0800

    implement add post feature

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useAppDispatch } from '@/app/hooks'
+import { Post, postAdded } from '@/features/posts/postsSlice'
+import { nanoid } from '@reduxjs/toolkit'
 
 // TS types for the input fields
 // See: https://epicreact.dev/how-to-type-a-react-form-on-submit-handler/
@@ -11,6 +14,9 @@ interface AddPostFormElements extends HTMLFormElement {
 }
 
 export const AddPostForm = () => {
+  // Get the `dispatch` method from the store
+  const dispatch = useAppDispatch()
+
   const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
     // Prevent server submission
     e.preventDefault()
@@ -19,8 +25,13 @@ export const AddPostForm = () => {
     const title = elements.postTitle.value
     const content = elements.postContent.value
 
-    console.log('Values: ', { title, content })
-
+    // Create the post object and dispatch the `postAdded` action
+    const newPost: Post = {
+      id: nanoid(),
+      title,
+      content
+    }
+    dispatch(postAdded(newPost))
     e.currentTarget.reset()
   }
 
