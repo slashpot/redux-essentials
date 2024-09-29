16284cae4cf5080a05c2545ad0818db91b48b2ad
Eric Lin <tttttt123465@gmail.com>
2024-09-29 14:13:15 +0800

    add auth reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,11 @@
 import { Action, configureStore } from '@reduxjs/toolkit'
 import { postsReducer } from '@/features/posts/postsSlice'
 import { usersReducer } from '@/features/users/usersSlice'
+import { authReducer } from '@/features/auth/authSlice'
 
 interface CounterState {
   value: number
 }
-
 
 function counterReducer(state: CounterState = { value: 0}, action: Action) {
   switch (action.type) {
@@ -19,6 +19,7 @@ export const store = configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    auth: authReducer,
   }
 })
 
